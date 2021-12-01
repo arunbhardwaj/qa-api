e58345ed7aaa2d498f28140cc1d1abce4b01e5b3
Arun Bhardwaj <arun.b.95@gmail.com>
2021-11-30 20:38:30 -0500

    refactores exports

--- server/db/db_questions.js
+++ server/db/db_questions.js
@@ -1,50 +1,101 @@
 const { client, pool } = require('./pgsql.js')
-const { questionResFormatter } = require('./responseFormatters.js');
+const { questionResFormatter, formatSubQueryAgg } = require('./responseFormatters.js');
 
 async function getAllQuestions(productId, page=1, count=5) {
+  // 2 seconds
+  // 10-20 milliseconds
+  // 40 millisecond cold
   let query = {
     text: 'SELECT * FROM Questions WHERE product_id = $1 ORDER BY question_helpfulness DESC LIMIT $2',
-    // 'SELECT \
-    //   q1.*, \
-    //   Answers.id as ans_id, \
-    //   Answers.body, \
-    //   Answers.date, \
-    //   Answers.answerer_name, \
-    //   Answers.answerer_email, \
-    //   Answers.helpfulness, \
-    //   Answers.question_id, \
-    //   Answers.reported as ans_reported\
-    // FROM (\
-    //   SELECT * FROM Questions WHERE product_id = $1 ORDER BY question_helpfulness DESC LIMIT $2 \
-    //   ) AS q1 INNER JOIN Answers ON q1.id = Answers.question_id'
     values: [productId, count],
   }
 
-  let results = await pool
-    .query(query)
-    .then(res => res.rows)
-    .catch(err => null)
-  let questions = results.map(({id}) => id);
-  let answers = await this.getAnswersForQuestions(questions);
-  results = results.map((question, idx) => {return {...question, answers: answers[idx]}})
-  results = questionResFormatter(productId, results);
+  // 3-3.5 Second average
+  // 4-6 milliseconds
+  // 35 milliseconds cold
+  let leftJoinQuery = {
+    text: `SELECT qa.*, photos.id as photos_id, photos.url as photos_url FROM (
+          SELECT
+            q1.*,
+            Answers.id as ans_id,
+            Answers.body,
+            Answers.date,
+            Answers.answerer_name,
+            Answers.answerer_email,
+            Answers.helpfulness,
+            Answers.question_id,
+            Answers.reported as ans_reported
+          FROM (
+            SELECT * FROM Questions WHERE product_id = $1 ORDER BY question_helpfulness DESC LIMIT $2
+          )
+          AS q1 LEFT JOIN Answers ON q1.id = Answers.question_id ORDER BY question_helpfulness DESC, helpfulness DESC
+          ) AS qa LEFT JOIN Photos ON qa.ans_id = Photos.answer_id
+    `,
+    values: [productId, count]
+  }
+
+  // 4-4.5 seconds
+  // After index on photos -> 4-5 milliseconds warm/consistent
+  // 25 milliseconds cold.
+  let joinQueryWithAgg = {
+    text: `
+      SELECT id, question_body, question_date, asker_name, asker_email, question_helpfulness, reported,
+        (
+          SELECT array_to_json(array_agg(a))
+          FROM (
+            SELECT id, body, date, answerer_name, answerer_email, helpfulness, question_id, reported,
+            (
+              SELECT array_to_json(array_agg(p))
+              FROM (
+                SELECT id, url
+                FROM Photos
+                WHERE Photos.answer_id = Answers.id
+              ) p
+            ) as photos
+            FROM Answers
+            WHERE Answers.question_id = Questions.id
+            ORDER BY Answers.helpfulness DESC
+          ) a
+        ) as answers
+      FROM Questions
+      WHERE Questions.product_id = $1 ORDER BY Questions.question_helpfulness DESC LIMIT $2
+    `,
+    values: [productId, count]
+  }
+
+  // let results = await pool
+  //   .query(query)
+  //   .then(res => res.rows)
+  //   .catch(err => null)
+  // let questions = results.map(({id}) => id);
+  // let answers = await this.getAllAnswersForAllQuestions(questions);
+  // results = results.map((question, idx) => {return {...question, answers: answers[idx]}})
+  // results = questionResFormatter(productId, results);
+
+
+  // let innerJoinResults = await pool.query(innerJoinQuery).then(res => res.rows).catch(err => console.error(err));
+  // let leftJoinResults = await pool.query(leftJoinQuery).then(res => res.rows).catch(err => console.error(err));
+  let joinQueryWithAggResults = await pool.query(joinQueryWithAgg).then(res => res.rows).catch(err => console.error(err));
+  // console.log(JSON.stringify(results, null, 2));
+  // console.log('///////////////////////////////////////\n///////////////////////////////\n////////////////////');
+  // console.log(JSON.stringify(leftJoinResults, null, 2));
+  // console.log('///////////////////////////////////////\n///////////////////////////////\n////////////////////');
+  // console.log(JSON.stringify(joinQueryWithAggResults, null, 2));
+  // let results = leftJoinResults
+  let results = formatSubQueryAgg(productId, joinQueryWithAggResults);
   return results;
 }
-module.exports.getAllQuestions = getAllQuestions
 
-
-async function getAnswersForQuestions(questionIds) {
+async function getAllAnswersForAllQuestions(questionIds) {
   let results = []
   for (const questionId of questionIds) {
-    let result = await module.exports.getAnswersForOneQuestion(questionId)
+    let result = await getAllAnswersForOneQuestion(questionId)
     results.push(result)
   }
   return results
 }
-module.exports.getAnswersForQuestions = getAnswersForQuestions
 
-
-async function getAnswersForOneQuestion(questionId, page = 1, count = 5) {
+async function getAllAnswersForOneQuestion(questionId, page = 1, count = 5) {
   let query = {
     text: 'SELECT * FROM Answers WHERE question_id = $1 ORDER BY helpfulness DESC LIMIT $2',
     values: [questionId, count],
@@ -52,67 +103,55 @@ async function getAnswersForOneQuestion(questionId, page = 1, count = 5) {
   let results = await pool.query(query).then(results => results.rows).catch(err => console.error(err));
 
   for (let answer of results) {
-    let photos = await module.exports.getPhotosForAnswer(answer.id).then(results => results.rows).catch(err => console.error(err));
+    let photos = await getPhotosForAnswer(answer.id).then(results => results.rows).catch(err => console.error(err));
     answer.photos = photos;
   }
 
   return results;
 }
-module.exports.getAnswersForOneQuestion = getAnswersForOneQuestion;
 
 
-module.exports.getPhotosForAnswer = (answerId) => {
+async function getPhotosForAnswer(answerId) {
   return pool.query({
     text: 'SELECT * FROM Photos WHERE answer_id = $1',
     values: [answerId]
   })
 }
 
-module.exports.updateQuestionHelpful = (questionId) => {
+async function updateQuestionHelpful(questionId) {
   return pool.query({
     text: 'UPDATE Questions SET question_helpfulness = question_helpfulness + 1 WHERE id = $1',
     values: [questionId]
   })
 }
 
-module.exports.updateAnswerHelpful = (answerId) => {
+async function updateAnswerHelpful(answerId) {
   return pool.query({
     text: 'UPDATE Answers SET helpfulness = helpfulness + 1 WHERE id = $1',
     values: [answerId]
   })
 }
 
-module.exports.saveQuestion = ({body, date, name, email, question_helpfulness, reported, product_id}) => {
+module.exports.saveQuestion = async ({body, date, name, email, question_helpfulness, reported, product_id}) => {
   return pool.query({
     text: 'INSERT INTO Questions(question_body, question_date, asker_name, asker_email, question_helpfulness, reported, product_id) VALUES ($1, $2, $3, $4, $5, $6, $7)',
     values: [body, date, name, email, question_helpfulness, reported, product_id]
   })
-  /*
-  id SERIAL NOT NULL PRIMARY KEY UNIQUE,
-  question_body VARCHAR(1000) NOT NULL,
-  question_date TIMESTAMPTZ NOT NULL,
-  asker_name VARCHAR(60) NOT NULL,
-  asker_email VARCHAR(60) NOT NULL,
-  question_helpfulness INTEGER NOT NULL DEFAULT 0,
-  reported BOOLEAN NOT NULL DEFAULT false,
-  product_id INTEGER NOT NULL
-  */
 }
 
-module.exports.saveAnswer = ({body, name, email, date, helpfulness, question_id, reported}) => {
+module.exports.saveAnswer = async ({body, name, email, date, helpfulness, question_id, reported}) => {
   return pool.query({
     text: 'INSERT INTO Answers(body,date,answerer_name,answerer_email,helpfulness,question_id,reported) \
       VALUES ($1,$2,$3,$4,$5,$6,$7)',
     values: [body, date, name, email, helpfulness, question_id, reported]
   })
-  /*
-  id SERIAL NOT NULL PRIMARY KEY UNIQUE,
-  body VARCHAR(1000) NOT NULL,
-  date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  answerer_name VARCHAR(60) NOT NULL,
-  answerer_email VARCHAR(60) NOT NULL,
-  helpfulness INTEGER NOT NULL DEFAULT 0,
-  question_id INTEGER NOT NULL,
-  reported BOOLEAN NOT NULL DEFAULT false
-  */
+}
+
+module.exports = {
+  getAllQuestions,
+  getAllAnswersForAllQuestions,
+  getAllAnswersForOneQuestion,
+  getPhotosForAnswer,
+  updateQuestionHelpful,
+  updateAnswerHelpful
 }
