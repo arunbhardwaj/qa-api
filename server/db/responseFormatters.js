1bfbbde7b425c06a5e07a3f214f8fad51ec8a7c7
Arun Bhardwaj <arun.b.95@gmail.com>
2021-11-30 20:42:51 -0500

    adds new formatting for the new left join and subquery queries

--- server/db/responseFormatters.js
+++ server/db/responseFormatters.js
@@ -1,5 +1,5 @@
 
-const questionResFormatter = (productId, questionArr) => {
+const combineAndFormatQuestionWithAnswers = (productId, questionArr) => {
   let res = {
     product_id: productId.toString(),
     results: questionArr.map(({id, question_body, question_date, asker_name, asker_email, question_helpfulness, reported, answers}) => {
@@ -29,6 +29,59 @@ const questionResFormatter = (productId, questionArr) => {
   return res;
 }
 
+const formatLeftJoinQuestions = (productId, questionArr) => {
+  let result = {
+    product_id: product_id.toString(),
+    results: questionArr.reduce((allQuestions, question) => {
+      if (!(allQuestions[question.id])) {
+        return Object.assign(allQuestions,
+          Object.assign(question.answers, {
+            [question.answer]: question.answer
+          })
+        )
+      }
+
+      return Object.assign(allQuestions, {
+        [question.id]: 1
+      })
+    })
+  }
+}
+
+const formatSubQueryAgg = (productId, questionsArr) => {
+  let result = {
+    product_id: productId.toString(),
+    results: questionsArr.map(({id, question_body, question_date, asker_name, asker_email, question_helpfulness, reported, answers}) => {
+      return {
+        question_id: id,
+        question_body,
+        question_date,
+        asker_name,
+        question_helpfulness,
+        reported,
+        answers: (answers) ? answers.reduce((answerObj, {id, body, date, answerer_name, answerer_email, helpfulness, reported, photos}) => {
+          return Object.assign(answerObj,
+            {
+              [id]: {
+                id,
+                body,
+                date,
+                answerer_name,
+                answerer_email,
+                helpfulness,
+                reported,
+                photos: (photos) ? photos : []
+              }
+            }
+        )}, {}) : {}
+      }
+    })
+  }
+  return result;
+}
+
+
 module.exports = {
-  questionResFormatter
+  questionResFormatter: combineAndFormatQuestionWithAnswers,
+  formatSubQueryAgg
 }
